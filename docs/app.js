76c36a7b393fe1fcd444721acf6e7409f00b16b6
jippen <cheetahmorph@gmail.com>
2017-07-22 02:14:32 -0700

    Simple scanner for pete - take 1

--- docs/app.js
+++ docs/app.js
@@ -10,7 +10,7 @@ var app = new Vue({
     var self = this;
     self.scanner = new Instascan.Scanner({ video: document.getElementById('preview'), scanPeriod: 5 });
     self.scanner.addListener('scan', function (content, image) {
-      self.scans.unshift({ date: +(Date.now()), content: content });
+      self.scans.unshift({ date: +(Date.now()), content: content.split('/')[2] });
     });
     Instascan.Camera.getCameras().then(function (cameras) {
       self.cameras = cameras;
