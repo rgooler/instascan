c14802969e215b4dadef01aea1adb11ac8ddfbde
jippen <cheetahmorph@gmail.com>
2017-07-22 02:46:03 -0700

    And again

--- docs/app.js
+++ docs/app.js
@@ -10,7 +10,7 @@ var app = new Vue({
     var self = this;
     self.scanner = new Instascan.Scanner({ video: document.getElementById('preview'), scanPeriod: 5 });
     self.scanner.addListener('scan', function (content, image) {
-      self.scans.unshift({ date: +(Date.now()), content: content.replace('www.therarebarrel.com','trb.com') });
+      self.scans.unshift({ count: count+1, content: content.replace('https://www.therarebarrel.com/product/','') });
     });
     Instascan.Camera.getCameras().then(function (cameras) {
       self.cameras = cameras;
