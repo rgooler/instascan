6434acd769ab1e78ea255a0ee185b6b2511de090
jippen <cheetahmorph@gmail.com>
2017-07-22 03:01:50 -0700

    Asdfg

--- docs/app.js
+++ docs/app.js
@@ -1,5 +1,10 @@
 var app = new Vue({
   el: '#app',
+  filters: {
+      recordLength(result, key) {
+         this.$set(key, result.length)
+      }
+  },
   data: {
     scanner: null,
     activeCameraId: null,
@@ -10,7 +15,7 @@ var app = new Vue({
     var self = this;
     self.scanner = new Instascan.Scanner({ video: document.getElementById('preview'), scanPeriod: 5 });
     self.scanner.addListener('scan', function (content, image) {
-      self.scans.unshift({ count: count+1, content: content.replace('https://www.therarebarrel.com/product/','') });
+      self.scans.unshift({ date: +(Date.now()), content: content.replace('https://www.therarebarrel.com/product/','') });
     });
     Instascan.Camera.getCameras().then(function (cameras) {
       self.cameras = cameras;
