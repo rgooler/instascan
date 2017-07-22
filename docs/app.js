491bd2603fa6adfd1d126f54172e09ae689479d4
jippen <cheetahmorph@gmail.com>
2017-07-22 02:27:08 -0700

    Take 3

--- docs/app.js
+++ docs/app.js
@@ -10,13 +10,13 @@ var app = new Vue({
     var self = this;
     self.scanner = new Instascan.Scanner({ video: document.getElementById('preview'), scanPeriod: 5 });
     self.scanner.addListener('scan', function (content, image) {
-      self.scans.unshift({ date: +(Date.now()), content: content.split('/')[3] });
+      self.scans.unshift({ date: +(Date.now()), content: content.replace('www.therarebarrel.com','trb.com') });
     });
     Instascan.Camera.getCameras().then(function (cameras) {
       self.cameras = cameras;
       if (cameras.length > 0) {
-        self.activeCameraId = cameras[1].id;
-        self.scanner.start(cameras[1]);
+        self.activeCameraId = cameras[0].id;
+        self.scanner.start(cameras[0]);
       } else {
         console.error('No cameras found.');
       }
